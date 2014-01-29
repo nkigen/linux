77c1e2639ddb38f4edee562c2884c5d0860d9f7e
Nelson Kigen <nellyk89@gmail.com>
2014-01-29 13:46:26 +0100

    Created SCHED_DSS module file

---
+++ kernel/sched/dss.c
@@ -0,0 +1,8 @@
+/*
+ * DSS Scheduling Class (SCHED_DSS)
+ *
+ * Dynamic Sporadic Server as described by Spuri and Buttazzo 1994, 1996
+ *
+ * Copyright (C) 2014 Nelson Kigen <nellyk89@gmail.com> 
+ *
+ */
