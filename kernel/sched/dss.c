a1f188bc58d788ae85b3a24de2c5925dc003f786
Nelson Kigen <nellyk89@gmail.com>
2014-01-29 14:09:20 +0100

    Defined dss_sched_class

--- kernel/sched/dss.c
+++ kernel/sched/dss.c
@@ -6,3 +6,25 @@
  * Copyright (C) 2014 Nelson Kigen <nellyk89@gmail.com> 
  *
  */
+
+
+const struct sched_class dss_sched_class = {
+	.next 			= &dl_sched_class,
+	.enqueue_task		= enqueue_task_dss,
+	.dequeue_task		= dequeue_task_dss,
+	.yield_task		= yield_task_dss,
+
+	.check_preempt_curr	= check_preempt_curr_dss,
+
+	.pick_next_task		= pick_next_task_dss,
+	.put_prev_task		= put_prev_task_dss,
+
+#ifdef CONFIG_SMP
+
+#endif
+
+	.set_curr-task		= set_curr_task_dss,
+	.task_tick		=task_tick_dss,
+
+
+};
