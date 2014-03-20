8c157a63858a43ff44fdd34cb310857e54216e01
nelson kigen <nellyk89@gmail.com>
2014-03-20 12:24:52 +0100

    Added MACROS to differentiate periodic vs sporadic tasks

--- include/linux/sched/dss.h
+++ include/linux/sched/dss.h
@@ -8,6 +8,15 @@
 
 #define MAX_DSS_PRIO 		0
 
+/*
+ * DSS_SPORADIC: Used to identify Sporadic Tasks
+ * DSS_PERIODIC: Used to identify Periodic Tasks
+ * Also priorities of DSS_PERIODIC > DSS_SPORADIC
+ */
+
+#define DSS_SPORADIC		1
+#define DSS_PERIODIC		0
+
 static inline int dss_prio(int prio)
 {
     if(unlikely(prio < MAX_DSS_PRIO))
